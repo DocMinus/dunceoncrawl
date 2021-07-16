eb4909169345148c1a5e47b2613a6a46cab99577
DocMinus <alex@pharmakarma.org>
2021-07-16 09:50:22 +0200

    basic map, sprint 1

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
-// basic structure, for minimal terminal.
-// requires dependency bracket-lib = "~0.8.1"
+mod map;
+//prelude "pub" not necessary since in main
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+use prelude::*;
 
-use bracket_lib::prelude::*;
+struct State {
+    map: Map,
+}
 
-struct State {}
-impl GameState for State {
-    fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket Terminal!");
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
     }
 }
 
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.map.render(ctx);
+    }
+}
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
-        .with_title("Dungeon Crawler basic basic framework")
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
         .build()?;
 
-    main_loop(context, State{})
+    main_loop(context, State::new())
 }
